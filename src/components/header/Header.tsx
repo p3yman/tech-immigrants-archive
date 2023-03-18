a2c4681cfa3631e9d543d0245a67c74f857d5501
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-03-18 11:13:50 +0100

    Make header responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center shadow p-3 pr-6 h-24 fixed w-full bg-white z-50">
-      <div className="flex gap-2 items-center">
+    <header className="flex flex-col sm:flex-row justify-between items-center shadow p-3 pr-6 sm:h-24 fixed w-full bg-white z-50">
+      <div className="flex gap-2 items-center mb-3 sm:mb-0">
         <Image src="/logo.jpeg" alt="Tech Immigrents" width="64" height="64" />
         <h1 className="text-2xl font-medium">Tech Immigrants</h1>
       </div>
@@ -12,17 +12,17 @@ export const Header = () => {
           href={`https://www.youtube.com/channel/test`}
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded"
+          className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded text-sm sm:text-base"
         >
-          Visit YouTube Channel
+          <span className="hidden sm:inline-block">Visit</span> YouTube Channel
         </a>
         <a
           href={`https://t.me/telegram`}
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-sm sm:text-base"
         >
-          Join Telegram Channel
+          <span className="hidden sm:inline-block">Join</span> Telegram Channel
         </a>
       </div>
     </header>
