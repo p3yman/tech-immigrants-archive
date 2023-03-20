a3d2531a0bb37187ca3f4867eb7740fb915aa8f3
Mohamadreza Golbaba <r.golbaba@extratik.com>
2023-03-20 07:58:34 +0300

    update prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+    overrides: [
+        {
+            files: '*.md',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+    importOrder: ['^components/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
