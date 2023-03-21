0bd06a8c503b9ed647e07ce0dafacb4d436b9136
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-03-21 07:53:30 +0100

    Add import order

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    singleQuote: false,
-    trailingComma: 'all',
-    printWidth: 120,
-    tabWidth: 2,
-    useTabs: false,
-    bracketSpacing: true,
-    arrowParens: 'always',
-    endOfLine: 'auto',
-    overrides: [
-        {
-            files: '*.md',
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
-    importOrder: ['^components/(.*)$', '^[./]'],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+  importOrder: ['^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
