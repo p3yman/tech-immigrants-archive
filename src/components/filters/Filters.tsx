ebc3a4a5c62691145e1a19b601f6e9623256a169
erfan-goodarzi <devgoodarzi@protonmail.com>
2023-03-26 17:08:59 +0330

    fix sidebar responsive style

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -23,7 +23,7 @@ interface FiltersProps {
 
 export const Filters = ({ filters, onChange, onClear }: FiltersProps) => {
   return (
-    <aside className="flex w-80 flex-col justify-between border-b bg-gray-50 p-5 pb-10 sm:min-h-full sm:border-r sm:border-b-0">
+    <aside className="flex flex-col justify-between border-b bg-gray-50 p-5 pb-10 sm:min-h-full sm:w-80 sm:border-r sm:border-b-0">
       <section className="flex flex-col gap-6">
         <div>
           <FilterTitle title="Search by name" onClear={() => onClear('search')} showClear={!!filters.search} />
